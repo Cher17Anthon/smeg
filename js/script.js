9b7580ec3ff2042597b11cccdee66522bf485dc3
Cher17Anthon <tcherepkovanthon@gmail.com>
2024-01-22 14:03:09 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true
+    });
+  });
+
+  // $(document).ready(function(){
+  //   $(".owl-carousel-two").owlCarousel({
+  //     items: 3,
+  //     margin: 10,
+  //   });
+  // });
